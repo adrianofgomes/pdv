26b814084cb27cdcde47595af69b42041735e39d
adrianofgomes <adrianofgomes@gmail.com>
2025-09-02 19:09:59 +0000

    Alterar geração de id (pk) de Produtos

--- backend/src/main/java/br/com/pogger/pdvfree/produtos/ProdutosEntity.java
+++ backend/src/main/java/br/com/pogger/pdvfree/produtos/ProdutosEntity.java
@@ -17,7 +17,7 @@ public class ProdutosEntity {
     
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Version
