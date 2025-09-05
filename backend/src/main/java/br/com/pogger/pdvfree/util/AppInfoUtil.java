d06deceb029342eb1a91dd6b2dd4073e539af063
adrianogomes <adrianofgomes@gmail.com>
2025-09-05 12:49:12 -0300

    Inclusão de actuator para monitoração da aplicação

---
+++ backend/src/main/java/br/com/pogger/pdvfree/util/AppInfoUtil.java
@@ -0,0 +1,20 @@
+package br.com.pogger.pdvfree.util;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppInfoUtil {
+    private final BuildProperties buildProperties;
+
+    public AppInfoUtil(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    public String getVersao() {
+        return buildProperties.getVersion();
+    }
+
+    public String getNomeAplicacaoVersao() {
+        return buildProperties.getName() + " - " + buildProperties.getVersion() + " (" + buildProperties.getTime() + ")";
+    }
+}
--- backend/src/main/java/br/com/pogger/pdvfree/util/StatusRest.java
+++ backend/src/main/java/br/com/pogger/pdvfree/util/StatusRest.java
@@ -1,13 +1,17 @@
 package br.com.pogger.pdvfree.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatusRest {
 
+    @Autowired
+    private AppInfoUtil appInfoUtil;
+
     @GetMapping("/status")
     public String status() {
-        return "OK";
+        return appInfoUtil.getNomeAplicacaoVersao();
     }
 }
